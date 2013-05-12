ec8035844916f6689c5bdcdbca7f7c986ea693f6
savas <savasaydin@gmail.com>
2013-05-12 22:32:23 +0200

    refactored some aiming flexible design

--- src/murenzhuang.erl
+++ src/murenzhuang.erl
@@ -4,17 +4,43 @@
 
 -define(FILE_8, file_8).
 
-feature(root, Input) ->
-    io:format("~s", [Input]);
-feature(feature_1, Input) ->
-    Output = mutator_copy(Input),
-    feature(root, Output);
-feature(feature_2, Input) ->
-    Output = mutator_copy(Input),
-    file:write_file(?FILE_8, Output);
-feature(feature_3, Input) ->
-    Output = mutator_decrease(Input),
-    feature(root, Output).
+feature(Feature, Input) ->
+    Steps = [fun get_mutator_result/2,
+	     fun std_or_file_out/2],
+    chain(Steps, {Feature, Input}).
+
+get_mutator_result(Feature, Input) ->
+    MutatorFun = proplists:get_value(Feature, which_mutator()),
+    {Feature, MutatorFun(Input)}.
+
+which_mutator() ->
+    [{root, fun no_mutator/1},
+     {feature_1, fun mutator_copy/1},
+     {feature_2, fun mutator_copy/1},
+     {feature_3, fun mutator_decrease/1}
+    ].
+
+std_or_file_out(Feature,Output) ->
+    case get_file(Feature) of
+	undefined ->
+	    io:format("~s", [Output]);
+	File ->
+	    file:write_file(File, Output)
+    end.
+
+get_file(Feature) ->
+    proplists:get_value(Feature, which_file()).
+
+which_file() ->
+    [{feature_2, ?FILE_8}].
+
+no_mutator(Input) ->
+    Input.
+
+mutator_copy(Input) when length(Input) < 5 ->
+    [];
+mutator_copy([A,B,C,D,E | T]) ->
+    [D,D,D,D,A,B,B,C,C,C] ++ mutator_copy([E | T]).
 
 mutator_decrease(Input) when length(Input) < 4 ->
     [];
@@ -28,10 +54,10 @@ decrease($a) ->
 decrease(X) ->
     X-1.
 
-mutator_copy(Input) when length(Input) < 5 ->
-    [];
-mutator_copy([A,B,C,D,E | T]) ->
-    [D,D,D,D,A,B,B,C,C,C] ++ mutator_copy([E | T]).
-	 
+chain(Functions, {Feature, Input}) ->
+    lists:foldl(fun(F,{Key, I}) -> F(Key, I) end,
+		{Feature, Input},
+		Functions).
+
   
     
