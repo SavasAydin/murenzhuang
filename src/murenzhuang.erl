d6a9294c36e6814331cdd87e60354abfe20cc278
savas <savasaydin@gmail.com>
2013-05-12 18:06:09 +0200

    Added feature 1/2/3 with 15 unit tests

--- src/murenzhuang.erl
+++ src/murenzhuang.erl
@@ -2,5 +2,36 @@
  
 -export([feature/2]).
 
+-define(FILE_8, file_8).
+
 feature(root, Input) ->
-    io:format("~s", [Input]).
+    io:format("~s", [Input]);
+feature(feature_1, Input) ->
+    Output = mutator_copy(Input),
+    feature(root, Output);
+feature(feature_2, Input) ->
+    Output = mutator_copy(Input),
+    file:write_file(?FILE_8, Output);
+feature(feature_3, Input) ->
+    Output = mutator_decrease(Input),
+    feature(root, Output).
+
+mutator_decrease(Input) when length(Input) < 4 ->
+    [];
+mutator_decrease([A,_,C,D | T]) ->
+    lists:map(fun decrease/1, [A,C,D]) ++ mutator_decrease([D | T]).
+
+decrease($0) ->
+    $9;
+decrease($a) ->
+    $z;
+decrease(X) ->
+    X-1.
+
+mutator_copy(Input) when length(Input) < 5 ->
+    [];
+mutator_copy([A,B,C,D,E | T]) ->
+    [D,D,D,D,A,B,B,C,C,C] ++ mutator_copy([E | T]).
+	 
+  
+    
--- test/murenzhuang_tests.erl
+++ test/murenzhuang_tests.erl
@@ -7,23 +7,127 @@ all_feature_test_() ->
      fun setup/0,
      fun cleanup/1,
      [
-      fun root_feature_empty_list/1,
-      fun root_feature_some_letters_and_numbers/1
+      fun root_feature_input_is_empty_list/1,
+      fun root_feature_input_contains_letters_and_numbers/1,
+      fun feature_1_input_is_empty_list/1,
+      fun feature_1_input_size_less_than_one_transform_trigger/1,
+      fun feature_1_input_size_allows_one_transform/1,
+      fun feature_1_input_size_greater_than_one_transform_trigger/1,
+      fun feature_1_input_size_allows_two_transforms/1,
+      fun feature_2_input_is_empty_list/1,
+      fun feature_2_input_size_not_allow_transform_execution/1,
+      fun feature_2_input_size_allows_one_transform/1,
+      fun feature_3_input_is_empty_list/1,
+      fun feature_3_input_size_less_than_one_transform_trigger/1,
+      fun feature_3_no_boundry_input_and_size_allows_one_transform/1,
+      fun feature_3_boundry_input_and_size_allows_one_transform/1,
+      fun feature_3_mix_input_and_size_greater_than_one_transform_trigger/1
      ]
     }.
 
-root_feature_empty_list(FileHandler) ->
+root_feature_input_is_empty_list(FileHandler) ->
     fun() ->
 	    group_leader(FileHandler, self()),
 	    murenzhuang:feature(root, ""),
 	    assert_feature(<<"">>, stdout)
     end.
 
-root_feature_some_letters_and_numbers(FileHandler) ->
+root_feature_input_contains_letters_and_numbers(FileHandler) ->
     fun() ->
 	    group_leader(FileHandler, self()),
 	    murenzhuang:feature(root, "abc123"),
 	    assert_feature(<<"abc123">>, stdout)
+    end.
+
+feature_1_input_is_empty_list(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_1, ""),
+	    assert_feature(<<"">>, stdout)
+    end.
+
+feature_1_input_size_less_than_one_transform_trigger(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_1, "a1b2"),
+	    assert_feature(<<"">>, stdout)
+    end.
+
+feature_1_input_size_allows_one_transform(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_1, "a1b2c"),
+	    assert_feature(<<"2222a11bbb">>, stdout)
+    end.
+
+feature_1_input_size_greater_than_one_transform_trigger(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_1, "a1b2c3"),
+	    assert_feature(<<"2222a11bbb">>, stdout)
+    end.
+
+feature_1_input_size_allows_two_transforms(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_1, "a1b2c3d4e"),
+	    assert_feature(<<"2222a11bbb4444c33ddd">>, stdout)
+    end.
+
+feature_2_input_is_empty_list(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_2, ""),
+	    assert_feature(<<"">>, file_8)
+    end.
+
+feature_2_input_size_not_allow_transform_execution(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_2, "abcd"),
+	    assert_feature(<<"">>, file_8)
+    end.
+
+feature_2_input_size_allows_one_transform(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_2, "9z8y7"),
+	    assert_feature(<<"yyyy9zz888">>, file_8)
+    end.
+
+feature_3_input_is_empty_list(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_3, ""),
+	    assert_feature(<<"">>, stdout)
+    end.
+
+feature_3_input_size_less_than_one_transform_trigger(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_3, "aaa"),
+	    assert_feature(<<"">>, stdout)
+    end.
+
+feature_3_no_boundry_input_and_size_allows_one_transform(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_3, "2345"),
+	    assert_feature(<<"134">>, stdout)
+    end.
+
+feature_3_boundry_input_and_size_allows_one_transform(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_3, "aa00"),
+	    assert_feature(<<"z99">>, stdout)
+    end.
+
+feature_3_mix_input_and_size_greater_than_one_transform_trigger(FileHandler) ->
+    fun() ->
+	    group_leader(FileHandler, self()),
+	    murenzhuang:feature(feature_3, "ab01x"),
+	    assert_feature(<<"z90">>, stdout)
     end.
 
 setup() ->
